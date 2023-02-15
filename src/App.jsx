3c44079a5dada5e501d6eca399417a86bce2085a
Rooche Digital <hello@rooche.net>
2023-02-15 13:50:40 +0800

    resetting files

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import Home from "./pages/Home/Home";
-import Cppa from "./pages/Platforms/Cppa/Cppa";
-import Cso from "./pages/Platforms/Cso/Cso";
-import Iib from "./pages/Platforms/iib/Iib";
+// import Cppa from "./pages/Platforms/Cppa/Cppa";
+// import Cso from "./pages/Platforms/Cso/Cso";
+// import Iib from "./pages/Platforms/iib/Iib";
 import "./App.css";
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
     <ChakraProvider>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="cppa" element={<Cppa />} />
+        {/* <Route path="cppa" element={<Cppa />} />
         <Route path="cso" element={<Cso />} />
-        <Route path="iib" element={<Iib />} />
+        <Route path="iib" element={<Iib />} /> */}
       </Routes>
     </ChakraProvider>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,43 +1,10 @@
 import { useNavigate } from "react-router-dom";
-import CustomContainer from "../../utils/Container/CustomContainer";
-import HomeButton from "../../utils/Button/HomeButton";
 import { Box, Heading, Stack, Text } from "@chakra-ui/react";
 
 const Home = () => {
   const navigate = useNavigate();
 
-  return (
-    <CustomContainer background_Color="linear(to-b, #0D31D1, #132885)">
-      <Box color="#fff">
-        <Box textAlign="center" maxWidth="18.688rem">
-          <Stack spacing="10" mb="12vh">
-            <Heading fontSize="3rem">ieFundEn</Heading>
-            <Stack spacing="1rem">
-              <Text fontWeight="700">Welcome</Text>
-              <Text lineHeight="1.3">
-                Welcome Nunc vulputate libero et velit interdum, ac aliquet odio
-                mattis.
-              </Text>
-            </Stack>
-          </Stack>
-          <Stack spacing="2.5">
-            <HomeButton
-              button_Name="C/PPA Platform"
-              onClick={() => navigate("cppa")}
-            />
-            <HomeButton
-              button_Name="CSO Wallet"
-              onClick={() => navigate("cso")}
-            />
-            <HomeButton
-              button_Name="IIB Portfolio"
-              onClick={() => navigate("iib")}
-            />
-          </Stack>
-        </Box>
-      </Box>
-    </CustomContainer>
-  );
+  return <Box bgColor="red">Hello</Box>;
 };
 
 export default Home;
